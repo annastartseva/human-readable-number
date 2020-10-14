862d5726fbde0f06c46790a14110c47d84a32970
Anna Startseva <a.v.starceva@gmail.com>
2020-10-15 00:44:17 +0600

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const unit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    const dozens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let result = '';
+    let k = 0;
+    let arr = [];
+    //console.log('number: ' + number);
+    if (number <= 20) {
+        result = unit[number];
+    } else {
+        while (number >= 1) {
+            arr[k] = (number - number % 1) % 10;
+            number /= 10;
+            k++;
+        }
+        //console.log('arr[] ' + arr);
+        for (let index = 0; index < arr.length; index++) {
+            let element = arr[index];
+            // console.log(element);
+            if (index === 0 && element !== 0) {
+                result = unit[element];
+            } else if (index === 1 && element > 1 && result !== '') {
+                result = `${dozens[element-1]} ${result}`;
+            } else if (index === 1 && element > 1 && result === '') {
+                result = `${dozens[element-1]}`;
+            } else if (index === 1 && element === 1) {
+                let y = `${arr[1]}${arr[0]}`;
+                // console.log('y ' + y);
+                result = `${unit[y]}`;
+            } else if (index === 2 && result !== '') {
+                result = `${unit[element]} hundred ${result}`;
+            } else if (index === 2 && result === '') {
+                result = `${unit[element]} hundred`;
+            }
+            //console.log('result ' + result);  
+        }
+    }
+    return result;
 }
